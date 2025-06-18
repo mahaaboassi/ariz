c24bff5d18d18d7153bc7165cdc2ec18e816403f
Maha Abo Assi <maha@onekw.com>
2025-06-18 22:00:46 +0400

    enhaced the profile Majid on home page

--- src/pages/home/sections/founder.jsx
+++ src/pages/home/sections/founder.jsx
@@ -16,9 +16,9 @@ const Founder = ()=>{
                 </div>
             </div>
             
-            <div className="content-founder ">
+            <div className="content-founder  ">
                 <div className="img-content"><img src={shape} alt="shape" /></div>
-                <div className="content p-5 lg:p-10 flex flex-col gap-5">
+                <div className="content h-full flex flex-col justify-center p-5 lg:p-10 gap-5 lg:gap-10">
                     <h4>Majid Ahmad – Founder & Marketing Strategist</h4>
                     <p>Leading with Passion, Delivering with Purpose</p>
                     <p>With 10+ years of hands-on experience in digital marketing, Majid Ahmad is the strategic mind behind Ariz Global’s impact across Dubai and the GCC. Recognized for his forward-thinking leadership and data-driven approach, Majid empowers brands through result-oriented marketing strategies. His reputation for integrity, insight, and innovation has made him a trusted advisor to both startups and established businesses navigating the digital landscape.</p>
